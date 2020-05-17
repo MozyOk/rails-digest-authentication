1c93979143ef65fe514ae1ccda596d7351da3595
mozy okubo <mozy.okubo@gmail.com>
2020-05-17 15:54:19 +0900

    add  digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
+require 'digest/md5'
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  REALM = 'SecretZone'
+  USERS = { 'user1' => Digest::MD5.hexdigest(['user1', REALM, 'password'].join(':')) }.freeze
+
+  def index
+    render plain: 'ログイン後に表示されるよ!'
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest(REALM) do |username|
+      USERS[username]
+    end
+  end
 end
